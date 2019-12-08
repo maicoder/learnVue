bfae812e2bc8e5747259eff99590e26f8b551f28
maicoder <hkdjcmc@msn.cn>
2019-12-09 01:25:27 +0800

    Update mmm.js

--- 12-前端模块化/02-ES6的模块化实现/mmm.js
+++ 12-前端模块化/02-ES6的模块化实现/mmm.js
@@ -1,3 +1,21 @@
+// 1.导入的{} 中定义的变量
+import {flag, sum} from "./aaa.js";
+
 if (flag) {
   console.log('xiaomingshitiancai');
+  console.log(sum(20, 30));;
 }
+
+// 2.直接导入export定义的变量
+import {num1, height} from "./aaa.js";
+
+console.log(num1);
+console.log(height);
+
+// 3.导入export的function
+import {mul, Person} from "./aaa.js";
+
+console.log(mul(30, 50));
+
+const p = new Person();
+p.run();
