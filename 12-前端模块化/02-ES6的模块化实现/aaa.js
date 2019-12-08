9d7f1037b080a98c8333a47fb24c9f286d46034b
maicoder <hkdjcmc@msn.cn>
2019-12-09 01:25:21 +0800

    Update aaa.js

--- 12-前端模块化/02-ES6的模块化实现/aaa.js
+++ 12-前端模块化/02-ES6的模块化实现/aaa.js
@@ -10,3 +10,22 @@ if (flag) {
   console.log(sum(20, 30));
 }
 
+// 1. 导出方式一
+export {
+  flag, sum
+}
+
+// 2. 导出方式二
+export var num1 = 1000;
+export var height = 1.88;
+
+// 3. 导出方式函数/类
+export function mul(num1, num2) {
+  return num1 * num2;
+}
+
+export class Person {
+  run() {
+    console.log('running');
+  }
+}
