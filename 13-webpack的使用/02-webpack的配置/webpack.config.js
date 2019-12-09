e1cfd2111853e6eee23b78006288c455cb4cebe2
maicoder <hkdjcmc@msn.cn>
2019-12-09 23:17:30 +0800

    Create webpack.config.js

---
+++ 13-webpack的使用/02-webpack的配置/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+// output 的path动态获取
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+}
