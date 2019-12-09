49bb0421bec959af3234a2f3480030b0b0135ef7
maicoder <hkdjcmc@msn.cn>
2019-12-10 00:01:43 +0800

    Create webpack.config.js

---
+++ 13-webpack的使用/03-webpack的loader/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+// output 的path动态获取
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // css-loader 只负责将css文件加载
+        // style-loader 负责将样式添加到DOM中
+        // 使用多个loader时，是从右向左
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  }
+}
