46bef3c9e550d42d7abb4bcfd3756fdb6278b16a
maicoder <hkdjcmc@msn.cn>
2019-12-10 00:01:13 +0800

    Update main.js

--- 13-webpack的使用/02-webpack的配置/src/main.js
+++ 13-webpack的使用/02-webpack的配置/src/main.js
@@ -1,11 +1,11 @@
 // 使用 CommonJS 的模块化规范
-const {add, mul} = require('./mathUtils.js')
+const {add, mul} = require('./js/mathUtils.js')
 
 console.log(add(20, 30));
 console.log(mul(20, 30));
 
 // 使用 ES6 的模块化规范
-import {name, age, height} from "./info";
+import {name, age, height} from "./js/info";
 
 console.log(name);
 console.log(age);
