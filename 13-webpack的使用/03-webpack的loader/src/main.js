e47527fe61c3936aee326da731bc4407b6a9a57a
maicoder <hkdjcmc@msn.cn>
2019-12-10 00:01:39 +0800

    Create main.js

---
+++ 13-webpack的使用/03-webpack的loader/src/main.js
@@ -0,0 +1,15 @@
+// 使用 CommonJS 的模块化规范
+const {add, mul} = require('./js/mathUtils.js')
+
+console.log(add(20, 30));
+console.log(mul(20, 30));
+
+// 使用 ES6 的模块化规范
+import {name, age, height} from "./js/info";
+
+console.log(name);
+console.log(age);
+console.log(height);
+
+// 3.依赖css文件
+require('./css/normal.css')
