83932dfdd3b955831c6c86e7c01dd8556bef88e5
maicoder <hkdjcmc@msn.cn>
2019-12-15 19:00:19 +0800

    Update index.js

--- 16-vue-router/learnvuerouter/src/router/index.js
+++ 16-vue-router/learnvuerouter/src/router/index.js
@@ -1,14 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
+import Home from '../components/Home'
+import About from "../components/About";
 
 // 1. 通过 Vue.use(插件)，安装插件
 Vue.use(Router)
 
 // 2. 创建 Router 对象
-const routes = []
-const router = new router({
-  routes
+const routes = [
+  {
+    path: '',   // 可以加或不加'/'
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/about',
+    component: About
+  }
+]
+const router = new Router({
+  // 配置路由和组件之间的映射关系
+  routes, // 默认是 url.hash
+  mode: 'history'
 })
 
 // 3. 将 router 对象传入到 Vue 实例
