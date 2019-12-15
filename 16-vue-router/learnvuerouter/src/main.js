8b3fdcf9f215fcd8db2e3037c2ba316df2d14fd6
maicoder <hkdjcmc@msn.cn>
2019-12-15 18:31:26 +0800

    Update Init Project

--- 16-vue-router/learnvuerouter/src/main.js
+++ 16-vue-router/learnvuerouter/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router'   // router 是文件夹，会自动查找 index 文件，所以可以忽略不写
 
 Vue.config.productionTip = false
 
--- 16-vue-router/learnvuerouter/src/router/index.js
+++ 16-vue-router/learnvuerouter/src/router/index.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 
+// 1. 通过 Vue.use(插件)，安装插件
 Vue.use(Router)
 
+// 2. 创建 Router 对象
+const routes = []
+const router = new router({
+  routes
+})
+
+// 3. 将 router 对象传入到 Vue 实例
+export default router
+
+/*
 export default new Router({
+  // 配置路由和组件之间的映射关系
   routes: [
     {
       path: '/',
@@ -13,3 +25,4 @@ export default new Router({
     }
   ]
 })
+*/
