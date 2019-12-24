04b5def29f9bace09dab3683f997a7786383e14f
maicoder <hkdjcmc@msn.cn>
2019-12-25 02:45:23 +0800

    Update App.vue

--- 16-vue-router/tabbar/src/App.vue
+++ 16-vue-router/tabbar/src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <div id="tab-bar">
+      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
+    </div>
   </div>
 </template>
 
@@ -13,5 +19,23 @@ export default {
 </script>
 
 <style>
+  @import "./assets/css/base.css";
 
+  #tab-bar {
+    display: flex;
+    background-color: #f6f6f6;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    box-shadow: 0 -1px 1px rgba(100,100,100,.2);
+  }
+
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
 </style>
