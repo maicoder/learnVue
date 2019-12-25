e70d273d73b1d4a6d2603fa903f13e2e18199457
maicoder <hkdjcmc@msn.cn>
2019-12-26 00:03:24 +0800

    Refactor Tabbar

--- 16-vue-router/tabbar/src/components/tabbar/TabBarItem.vue
+++ 16-vue-router/tabbar/src/components/tabbar/TabBarItem.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="tab-bar-item">
+  <div class="tab-bar-item" @click="itemClick">
 <!--    <slot v-if="!isActive" name="item-icon"></slot>-->
 <!--    <slot v-else name="item-icon-active"></slot>-->
     <div v-if="!isActive"><slot name="item-icon"></slot></div>
     <div v-else><slot name="item-icon-active"></slot></div>
-    <div :class="{active: isActive}">
+    <div :style="activeStyle">
       <slot name="item-text"></slot>
     </div>
 
@@ -14,9 +14,29 @@
 <script>
   export default {
     name: "TabBarItem",
+    props: {
+      path: String,
+      activeColor: {
+        type: String,
+        default: 'red'
+      }
+    },
     data() {
       return {
-        isActive: true
+        // isActive: true
+      }
+    },
+    computed: {
+      isActive() {
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeStyle() {
+        return this.isActive ? {color: this.activeColor} : {}
+      }
+    },
+    methods: {
+      itemClick() {
+        this.$router.replace(this.path)
       }
     }
   }
@@ -36,9 +56,5 @@
     margin-top: 3px;
     vertical-align: middle;
     margin-bottom: 2px;
-  }
-
-  .active {
-    color: red;
   }
 </style>
