335d8200c9545a4c0e3073a7fc898bd90411f80b
maicoder <hkdjcmc@msn.cn>
2019-12-26 22:46:10 +0800

    Add data control base

--- 17-vuex/learnvuex/src/App.vue
+++ 17-vuex/learnvuex/src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
+    <h2>{{message}}</h2>
+    <h2>{{counter}}</h2>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+
+    <hello-vuex :counter="counter"/>
   </div>
 </template>
 
 <script>
+  import HelloVuex from "./components/HelloVuex";
 
-export default {
-  name: 'App',
-  components: {
-
+  export default {
+    name: 'App',
+    components: {
+      HelloVuex
+    },
+    data() {
+      return {
+        message: '我是App组件',
+        counter: 0
+      }
+    }
   }
-}
 </script>
 
 <style>
-  
+
 </style>
