3819b587b6303018ba94d66e90ee5526c818b19a
maicoder <hkdjcmc@msn.cn>
2019-12-30 16:55:57 +0800

    learn axios

--- 18-axios/learnaxios/src/main.js
+++ 18-axios/learnaxios/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -8,4 +9,39 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+// axios({
+//   // url: 'httpbin.org'
+//   url: 'http://123.207.32.32:8000/home/multidata'
+// }).then(res => {
+//   console.log(res);
+// })
+
+import {request} from "./network/request";
+
+// request({
+//   baseConfig: {
+//
+//   },
+//   success: function (res) {
+//
+//   },
+//   failure: function (err) {
+//
+//   }
+// })
+
+// request({
+//   url: 'http://123.207.32.32:8000'
+// }).then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// })
+
+request({
+  url: '/home/multidata'
+}, res => {
+  console.log(res);
 })
