0386c253710fe4051106e917e5cebea2f7956c2f
maicoder <hkdjcmc@msn.cn>
2020-01-07 08:39:04 +0800

    Update Scroll.vue

--- 19-mall/supermall/src/components/common/scroll/Scroll.vue
+++ 19-mall/supermall/src/components/common/scroll/Scroll.vue
@@ -39,10 +39,11 @@
 
       // 2. 监听滚动的位置
       this.scroll.on('scroll', (position) => {
-        console.log(position);
+        // console.log(position);
         this.$emit('scroll', position)
       })
 
+      // 3. 监听上拉加载
       this.scroll.on('pullingUp', () => {
         this.$emit('pullingUp')
       })
