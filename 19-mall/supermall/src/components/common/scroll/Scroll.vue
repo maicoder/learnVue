2beda6063c86ca4fcf93456774ed777e0db2ffc2
maicoder <hkdjcmc@msn.cn>
2020-01-08 16:40:18 +0800

    Update Scroll.vue

--- 19-mall/supermall/src/components/common/scroll/Scroll.vue
+++ 19-mall/supermall/src/components/common/scroll/Scroll.vue
@@ -37,16 +37,20 @@
         pullUpLoad: this.pullUpLoad
       })
 
-      // 2. 监听滚动的位置
-      this.scroll.on('scroll', (position) => {
-        // console.log(position);
-        this.$emit('scroll', position)
-      })
+      if (this.probeType === 2 || this.probeType === 3) {
+        // 2. 监听滚动的位置
+        this.scroll.on('scroll', (position) => {
+          // console.log(position);
+          this.$emit('scroll', position)
+        })
+      }
 
       // 3. 监听上拉加载
-      this.scroll.on('pullingUp', () => {
-        this.$emit('pullingUp')
-      })
+      if (this.pullUpLoad) {
+        this.scroll.on('pullingUp', () => {
+          this.$emit('pullingUp')
+        })
+      }
     },
     methods: {
       scrollTo(x, y, time=300) {
