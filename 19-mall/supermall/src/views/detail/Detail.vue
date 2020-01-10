7246c91236b4a7c14c9809408bfa2a04e5161718
maicoder <hkdjcmc@msn.cn>
2020-01-10 21:44:03 +0800

    Update Detail.vue

--- 19-mall/supermall/src/views/detail/Detail.vue
+++ 19-mall/supermall/src/views/detail/Detail.vue
@@ -5,6 +5,7 @@
       <detail-swiper :top-images="topImages"/>
       <detail-base-info :goods="goods"/>
       <detail-shop-info :shop="shop"/>
+      <detail-goods-info :detail-info="detailInfo"/>
     </scroll>
   </div>
 </template>
@@ -14,6 +15,7 @@
   import DetailSwiper from './childComps/DetailSwiper'
   import DetailBaseInfo from './childComps/DetailBaseInfo'
   import DetailShopInfo from './childComps/DetailShopInfo'
+  import DetailGoodsInfo from './childComps/DetailGoodsInfo'
 
   import Scroll from 'components/common/scroll/Scroll'
 
@@ -26,14 +28,16 @@
       DetailSwiper,
       DetailBaseInfo,
       DetailShopInfo,
-      Scroll
+      Scroll,
+      DetailGoodsInfo
     },
     data() {
       return {
         iid: null,
         topImages: [],
         goods: {},
-        shop: {}
+        shop: {},
+        detailInfo: {}
       }
     },
     created() {
@@ -45,14 +49,17 @@
         // 1.获取顶部数据
         console.log(res);
         const data = res.result;
-        this.topImages = data.itemInfo.topImages
+        this.topImages = data.itemInfo.topImages;
         console.log(this.topImages);
 
         // 2. 获取商品信息
-        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services);
 
         // 3. 创建店铺信息的对象
-        this.shop = new Shop(data.shopInfo)
+        this.shop = new Shop(data.shopInfo);
+
+        // 4. 保存商品的详情数据
+        this.detailInfo = data.detailInfo;
       })
     }
   }
