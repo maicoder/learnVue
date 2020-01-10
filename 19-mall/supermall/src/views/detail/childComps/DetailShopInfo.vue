28a8a794e573e27424986e918a4671090f3bd5bd
maicoder <hkdjcmc@msn.cn>
2020-01-10 21:22:35 +0800

    Update DetailShopInfo.vue

--- 19-mall/supermall/src/views/detail/childComps/DetailShopInfo.vue
+++ 19-mall/supermall/src/views/detail/childComps/DetailShopInfo.vue
@@ -1,26 +1,32 @@
 <template>
-  <div class="detail-shopInfo">
-    <div class="shop-logo">
-      <img :src="shop.logo" alt="">
-      <span class="shop-name">{{shop.name}}</span>
+  <div class="shop-info">
+    <div class="shop-top">
+      <img :src="shop.logo">
+      <span class="title">{{shop.name}}</span>
     </div>
-    <div class="shop-other">
-      <div class="shop-sales">
-        <div class="shop-sell">
-          <span class="shop-num">{{shop.sells}}</span>
-          <span class="shop-text">总销量</span>
+    <div class="shop-middle">
+      <div class="shop-middle-item shop-middle-left">
+        <div class="info-sells">
+          <div class="sells-count">
+            {{shop.sells | sellCountFilter}}
+          </div>
+          <div class="sells-text">总销量</div>
         </div>
-        <div class="shop-goods">
-          <span class="shop-num">{{shop.goodsCount}}</span>
-          <span class="shop-text">全部宝贝</span>
+        <div class="info-goods">
+          <div class="goods-count">
+            {{shop.goodsCount}}
+          </div>
+          <div class="goods-text">全部宝贝</div>
         </div>
       </div>
-      <div class="shop-description">
-        <div class="description-div" v-for="(item,index) in shop.score" :key="index">
-          <span>{{item.name}}</span>
-          <span :class="{highColor: item.isBetter, lowColor: !item.isBetter}">{{item.score}}</span>
-          <span :class="{high: item.isBetter, low: !item.isBetter}">{{item.isBetter? '高' : '低'}}</span>
-        </div>
+      <div class="shop-middle-item shop-middle-right">
+        <table>
+          <tr v-for="(item, index) in shop.score" :key="index">
+            <td>{{item.name}}</td>
+            <td class="score" :class="{'score-better': item.isBetter}">{{item.score}}</td>
+            <td class="better" :class="{'better-more': item.isBetter}"><span>{{item.isBetter ? '高':'低'}}</span></td>
+          </tr>
+        </table>
       </div>
     </div>
     <div class="shop-bottom">
@@ -39,91 +45,97 @@
           return {}
         }
       }
+    },
+    filters: {
+      sellCountFilter: function (value) {
+        if (value < 10000) return value;
+        return (value/10000).toFixed(1) + '万'
+      }
     }
   }
 </script>
 
 <style scoped>
-  .detail-shopInfo {
-    padding: 0 10px;
+  .shop-info {
+    padding: 25px 8px;
+    border-bottom: 5px solid #f2f5f8;
+    background-color: #fff;
   }
-  .shop-logo {
-    height: 100px;
-    padding: 20px 0;
+  .shop-top {
+    line-height: 45px;
+    /* 让元素垂直中心对齐 */
+    display: flex;
+    align-items: center;
   }
-  .shop-logo img {
-    height: 100%;
-    vertical-align: middle;
+  .shop-top img {
+    width: 45px;
+    height: 45px;
     border-radius: 50%;
+    border: 1px solid rgba(0,0,0,.1);
   }
-  .shop-name {
-    padding-left: 10px;
-    font-size: 16px;
+  .shop-top .title {
+    margin-left: 10px;
+    vertical-align: center;
   }
-  .shop-other {
+  .shop-middle {
+    margin-top: 15px;
     display: flex;
-    padding: 0 20px 20px;
+    align-items: center;
   }
-  .shop-sales, .shop-description {
+  .shop-middle-item {
     flex: 1;
   }
-  .shop-sales {
+  .shop-middle-left {
     display: flex;
-    border-right: 1px solid #ccc;
-    padding-right: 10px;
+    justify-content: space-evenly;
+    color: #333;
+    text-align: center;
+    border-right: 1px solid rgba(0,0,0,.1);
   }
-  .shop-sell, .shop-goods {
-    flex: 1;
-    display: flex;
-    /* flex-wrap: wrap;
-    align-content: space-between; */
-    flex-direction: column;
-    justify-content: space-around;
+  .sells-count, .goods-count {
+    font-size: 18px;
+  }
+  .sells-text, .goods-text {
+    margin-top: 10px;
+    font-size: 12px;
+  }
+  .shop-middle-right {
+    font-size: 13px;
+    color: #333;
+  }
+  .shop-middle-right table {
+    width: 120px;
+    margin-left: 30px;
+  }
+  .shop-middle-right table td {
+    padding: 5px 0;
+  }
+  .shop-middle-right .score {
+    color: #5ea732;
+  }
+  .shop-middle-right .score-better {
+    color: #f13e3a;
+  }
+  .shop-middle-right .better span {
+    background-color: #5ea732;
+    color: #fff;
     text-align: center;
   }
-  .shop-num {
-    font-size: 25px;
-  }
-  .shop-text {
-    font-size: 14px;
-  }
-  .shop-description {
-    padding-left: 10px;
-  }
-  .shop-description span:nth-child(1) {
-    margin-right: 16px;
-  }
-  .shop-description span:nth-child(3) {
-    float: right;
-  }
-  .description-div {
-    margin: 5px 0;
-  }
-  .highColor {
-    color: orangered;
-  }
-  .lowColor {
-    color: #54d425;
-  }
-  .high {
-    color: #fff;
-    background-color: orangered;
-  }
-  .low {
-    color: #fff;
-    background-color: #54d425;
+  .shop-middle-right .better-more span {
+    background-color: #f13e3a;
   }
   .shop-bottom {
-    height: 50px;
+    text-align: center;
+    margin-top: 10px;
   }
   .enter-shop {
-    width: 33%;
+    display: inline-block;
+    font-size: 14px;
+    background-color: #f2f5f8;
+    width: 150px;
     height: 30px;
     text-align: center;
     line-height: 30px;
-    margin: 10px auto;
-    border-radius: 6px;
-    /* border: 1px solid #ccc; */
-    background-color: #dad5d585;
+    border-radius: 10px;
   }
 </style>
