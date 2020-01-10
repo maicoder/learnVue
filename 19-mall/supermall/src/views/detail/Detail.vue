cd8625706e2b2fa6db2b6fc243fe4b658c56ef9a
maicoder <hkdjcmc@msn.cn>
2020-01-11 00:04:10 +0800

    Update Detail.vue

--- 19-mall/supermall/src/views/detail/Detail.vue
+++ 19-mall/supermall/src/views/detail/Detail.vue
@@ -6,6 +6,7 @@
       <detail-base-info :goods="goods"/>
       <detail-shop-info :shop="shop"/>
       <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
+      <detail-param-info :param-info="paramInfo"/>
     </scroll>
   </div>
 </template>
@@ -16,10 +17,11 @@
   import DetailBaseInfo from './childComps/DetailBaseInfo'
   import DetailShopInfo from './childComps/DetailShopInfo'
   import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+  import DetailParamInfo from './childComps/DetailParamInfo'
 
   import Scroll from 'components/common/scroll/Scroll'
 
-  import {getDetail, Goods, Shop} from 'network/detail'
+  import {getDetail, Goods, Shop, GoodsParam} from 'network/detail'
 
   export default {
     name: "Detail",
@@ -29,7 +31,8 @@
       DetailBaseInfo,
       DetailShopInfo,
       Scroll,
-      DetailGoodsInfo
+      DetailGoodsInfo,
+      DetailParamInfo
     },
     data() {
       return {
@@ -37,12 +40,14 @@
         topImages: [],
         goods: {},
         shop: {},
-        detailInfo: {}
+        detailInfo: {},
+        paramInfo: {}
       }
     },
     created() {
       // 1.保存传入的iid
-      this.iid = this.$route.params.iid
+      // this.iid = this.$route.params.iid;
+      this.iid = this.$route.query.iid;
 
       // 2.根据iid请求详情数据
       getDetail(this.iid).then(res => {
@@ -50,7 +55,7 @@
         console.log(res);
         const data = res.result;
         this.topImages = data.itemInfo.topImages;
-        console.log(this.topImages);
+        // console.log(this.topImages);
 
         // 2. 获取商品信息
         this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services);
@@ -60,6 +65,9 @@
 
         // 4. 保存商品的详情数据
         this.detailInfo = data.detailInfo;
+
+        // 5. 获取参数信息
+        this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
       })
     },
     methods: {
