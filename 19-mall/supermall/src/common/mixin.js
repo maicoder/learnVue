6bc8bbdd08777c0f30dcf22a9ad7ad30fee989e7
maicoder <hkdjcmc@msn.cn>
2020-01-11 06:24:48 +0800

    Update mixin.js

--- 19-mall/supermall/src/common/mixin.js
+++ 19-mall/supermall/src/common/mixin.js
@@ -1,6 +1,11 @@
 import {debounce} from "./utils";
 
 export const itemListenerMixin = {
+  data() {
+    return {
+      itemImgListener: null
+    }
+  },
   mounted() {
     let newRefresh = debounce(this.$refs.scroll.refresh, 100)
     this.itemImgListener = () => {
