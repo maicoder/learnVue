3aec61577d5a7df21f819e8176f3c741dfd0f401
maicoder <hkdjcmc@msn.cn>
2020-01-10 20:27:21 +0800

    Update Detail.vue

--- 19-mall/supermall/src/views/detail/Detail.vue
+++ 19-mall/supermall/src/views/detail/Detail.vue
@@ -2,25 +2,31 @@
   <div id="detail">
     <detail-nav-bar/>
     <detail-swiper :top-images="topImages"/>
+    <detail-base-info :goods="goods"/>
   </div>
 </template>
 
 <script>
   import DetailNavBar from './childComps/DetailNavBar'
   import DetailSwiper from './childComps/DetailSwiper'
+  import DetailBaseInfo from './childComps/DetailBaseInfo'
+  
 
-  import {getDetail} from 'network/detail'
+  import {getDetail, Goods, Shop} from 'network/detail'
 
   export default {
     name: "Detail",
     components: {
       DetailNavBar,
-      DetailSwiper
+      DetailSwiper,
+      DetailBaseInfo
     },
     data() {
       return {
         iid: null,
-        topImages: []
+        topImages: [],
+        goods: {},
+        shop: {}
       }
     },
     created() {
@@ -31,8 +37,15 @@
       getDetail(this.iid).then(res => {
         // 1.获取顶部数据
         console.log(res);
-        this.topImages = res.result.itemInfo.topImages
+        const data = res.result;
+        this.topImages = data.itemInfo.topImages
         console.log(this.topImages);
+
+        // 2. 获取商品信息
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+        // 3. 创建店铺信息的对象
+        this.shop = new Shop(data.shopInfo)
       })
     }
   }
