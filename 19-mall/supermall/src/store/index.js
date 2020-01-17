f3861bb5b09d9ec05822de8a08d6f82c114cae10
maicoder <hkdjcmc@msn.cn>
2020-01-17 22:15:40 +0800

    Update index.js

--- 19-mall/supermall/src/store/index.js
+++ 19-mall/supermall/src/store/index.js
@@ -10,24 +10,46 @@ const store = new Vuex.Store({
     carList: []
   },
   mutations: {
-    addCart(state, payload) {
-      // payload 新添加的商品
-      // let oldProduct = null;
-      // for (let item of state.cartList) {
-      //   if (item.iid === payload.iid) {
-      //     oldProduct = item;
-      //   }
-      // }
+    // mutations 唯一的目的就是修改 state 中状态
+    // mutations 中的每一个方法尽可能完成的事件比较单一一点
+    addCounter(state, payload) {
+      payload.count++;
+    },
+    addToCart(state, payload) {
+      state.carList.push(payload);
+    }
+  },
+  methods: {
+    // addCart(state, payload) {
+    //   // payload 新添加的商品
+    //   // let oldProduct = null;
+    //   // for (let item of state.cartList) {
+    //   //   if (item.iid === payload.iid) {
+    //   //     oldProduct = item;
+    //   //   }
+    //   // }
+    //
+    //   // let oldProduct = state.carList.find(item => item.iid === payload.iid);
+    //
+    //   // 2. 判断 oldProduct
+    //   // if (oldProduct) {
+    //   //   oldProduct.count += 1;
+    //   // } else {
+    //   //   payload.count = 1;
+    //   //   state.carList.push(payload);
+    //   // }
+    // }
+    addCart(context, payload) {
+      let oldProduct = context.state.carList.find(item => item.iid === payload.iid);
 
-      // let oldProduct = state.carList.find(item => item.iid === payload.iid);
-
-      // 2. 判断 oldProduct
-      // if (oldProduct) {
-      //   oldProduct.count += 1;
-      // } else {
-      //   payload.count = 1;
-      //   state.carList.push(payload);
-      // }
+      if (oldProduct) {
+        // oldProduct.count += 1;
+        context.commit('addCounter', oldProduct);
+      } else {
+        payload.count = 1;
+        // context.state.carList.push(payload);
+        context.commit('addToCart', payload);
+      }
     }
   }
 })
