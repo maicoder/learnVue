35b6d787edfa26d674ac053ed82b665f42a36b43
maicoder <hkdjcmc@msn.cn>
2020-01-18 20:37:19 +0800

    Update Cart.vue

--- 19-mall/supermall/src/views/cart/Cart.vue
+++ 19-mall/supermall/src/views/cart/Cart.vue
@@ -1,30 +1,40 @@
 <template>
   <div class="cart">
     <nav-bar class="nav-bar">
-      <div slot="center">购物车({{cartLength}})</div>
+      <div slot="center">购物车({{length}})</div>
     </nav-bar>
+
+    <cart-list/>
+
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import CartList from './childComps/CartList'
 
   import { mapGetters } from 'vuex'
 
   export default {
     name: "Cart",
     components: {
-      NavBar
+      NavBar,
+      CartList
     },
     computed: {
-      cartLength() {
-        return this.$store.getters.cartLength;
-      }
+      // 两种写法
+      // ...mapGetters(['cartLength'])
+      ...mapGetters({
+        length: 'cartLength'
+      })
     }
   }
 </script>
 
 <style scoped>
+  .cart {
+    height: 100vh;
+  }
   .nav-bar {
     background-color: var(--color-tint);
     color: #fff;
