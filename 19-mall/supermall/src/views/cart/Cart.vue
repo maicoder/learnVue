695a15f385cd73c3996b7fed5b804db844de7143
maicoder <hkdjcmc@msn.cn>
2020-01-30 17:08:37 +0800

    Update Cart.vue

--- 19-mall/supermall/src/views/cart/Cart.vue
+++ 19-mall/supermall/src/views/cart/Cart.vue
@@ -12,14 +12,16 @@
 <script>
   import NavBar from 'components/common/navbar/NavBar';
   import CartList from './childComps/CartList';
+  import CartBottomBar from './childComps/CartBottomBar';
 
-  import { mapGetters } from 'vuex'
+  import { mapGetters } from 'vuex';
 
   export default {
     name: "Cart",
     components: {
       NavBar,
       CartList,
+      CartBottomBar
     },
     computed: {
       // 两种写法
