b237a81b5ceb0c4d4e9c33f5bde8a2963fd6f854
maicoder <hkdjcmc@msn.cn>
2020-01-31 15:17:07 +0800

    Update Toast.vue

--- 19-mall/supermall/src/components/common/toast/Toast.vue
+++ 19-mall/supermall/src/components/common/toast/Toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast" v-show="show">
+  <div class="toast" v-show="isShow">
     <div>{{message}}</div>
   </div>
 </template>
@@ -8,13 +8,31 @@
   export default {
     name: "Toast",
     props: {
-      message: {
-        type: String,
-        default: ''
-      },
-      show: {
-        type: Boolean,
-        default: false
+      // message: {
+      //   type: String,
+      //   default: ''
+      // },
+      // show: {
+      //   type: Boolean,
+      //   default: false
+      // }
+    },
+    data() {
+      return {
+        message: '',
+        isShow: false
+      }
+    },
+    methods: {
+      show(message='默认文字', duration=2000) {
+        // duration = duration || 2000;
+        this.isShow = true;
+        this.message = message;
+
+        setTimeout(() => {
+          this.isShow = false;
+          this.message = ''
+        }, duration)
       }
     }
   }
@@ -27,6 +45,8 @@
     left: 50%;
     transform: translate(-50%, -50%);
     padding: 8px 10px;
+
+    z-index: 999;
 
     color: #ffffff;
     background-color: rgba(0, 0, 0, .75);
