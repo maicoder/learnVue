e19562ebe2f2b0765d45fe92ca3f4f3288f6bb07
maicoder <hkdjcmc@msn.cn>
2020-01-31 13:23:03 +0800

    Update actions.js

--- 19-mall/supermall/src/store/actions.js
+++ 19-mall/supermall/src/store/actions.js
@@ -5,13 +5,18 @@ import {
 
 export default {
   addCart(context, payload) {
-    let oldProduct = context.state.cartList.find(item => item.iid === payload.iid);
+    return new Promise((resolve, reject) => {
+      let oldProduct = context.state.cartList.find(item => item.iid === payload.iid);
 
-    if (oldProduct) {
-      context.commit(ADD_COUNTER, oldProduct);
-    } else {
-      payload.count = 1;
-      context.commit(ADD_TO_CART, payload);
-    }
+      if (oldProduct) {
+        context.commit(ADD_COUNTER, oldProduct);
+        resolve('当前商品的数量+1')
+      } else {
+        payload.count = 1;
+        context.commit(ADD_TO_CART, payload);
+        resolve('添加了新的商品')
+      }
+    })
+
   }
 }
