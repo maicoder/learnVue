3a4f543854e8000a7cceccec1a403a2834a98d86
maicoder <hkdjcmc@msn.cn>
2020-02-07 20:22:39 +0800

    Update TabControl.vue

--- 19-mall/supermall/src/components/content/tabControl/TabControl.vue
+++ 19-mall/supermall/src/components/content/tabControl/TabControl.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="tab-control">
-    <div v-for="(item, index) in titles"
-         class="tab-control-item"
+    <div v-for="(item, index) in titles" class="tab-control-item"
          :class="{active: index === currentIndex}" @click="itemClick(index)">
       <span>{{item}}</span>
     </div>
